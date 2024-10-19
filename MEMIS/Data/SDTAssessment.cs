66f3e845b5756233ea10e1c9eb1c77df0af9129e
Sreejith O.P. <sreejith@future.co.ug>
2024-10-19 10:37:08 +0300

    SDTAssessmentYearQuarter

--- MEMIS/Data/SDTAssessment.cs
+++ MEMIS/Data/SDTAssessment.cs
@@ -1,115 +1,117 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using MEMIS;
 namespace MEMIS.Data
 {
-    [Table("SDTAssessment")]
-    public class SDTAssessment
-    {
-        [Key]
-        public int Id { get; set; }
+  [Table("SDTAssessment")]
+  public class SDTAssessment
+  {
+    [Key]
+    public int Id { get; set; }
 
-        [Required]
-        public int Month { get; set; }
+    [Required]
+    public int Month { get; set; }
+    public int FYear { get; set; }
+    public int Quarter { get; set; }
 
-        [Required]
-        public float Numerator { get; set; }
+    [Required]
+    public float Numerator { get; set; }
 
-        [Required]
-        public float Denominator { get; set; }
+    [Required]
+    public float Denominator { get; set; }
 
-        [Display(Name ="Implemented within Timeline")]
-        [Required]
-        public float ImplementedTimeline { get; set; }
+    [Display(Name = "Implemented within Timeline")]
+    [Required]
+    public float ImplementedTimeline { get; set; }
 
-        [Required]
-		[Display(Name = "Average Days")]
-		public float Rate { get; set; }
+    [Required]
+    [Display(Name = "Average Days")]
+    public float Rate { get; set; }
 
-        [Display(Name = "Propotion implemented with Timelines")]
-        [Required]
-        public float ProportionTimeline { get; set; }
+    [Display(Name = "Propotion implemented with Timelines")]
+    [Required]
+    public float ProportionTimeline { get; set; }
 
-        [Required]
-        public int Target { get; set; }
+    [Required]
+    public int Target { get; set; }
 
-        [Display(Name = "Achivement Status")]
-        [Required]
-        public string? AchivementStatus { get; set; }
+    [Display(Name = "Achivement Status")]
+    [Required]
+    public string? AchivementStatus { get; set; }
 
-        [Display(Name = "Percentage Variance")]
-        [DisplayFormat(DataFormatString = "{0:P2}")]
-        [Required]
-        public string? Variance { get; set; }
+    [Display(Name = "Percentage Variance")]
+    [DisplayFormat(DataFormatString = "{0:P2}")]
+    [Required]
+    public string? Variance { get; set; }
 
-        [Required]
-        [MaxLength(500)]
-        public string? Justification { get; set; }
+    [Required]
+    [MaxLength(500)]
+    public string? Justification { get; set; }
 
-        [Required]
-        public string? Rating { get; set; }
+    [Required]
+    public string? Rating { get; set; }
 
-        [Display(Name = "HOD Action")]
-        public string? HODAction { get; set; }
+    [Display(Name = "HOD Action")]
+    public string? HODAction { get; set; }
 
-        [Display(Name = "HOD Comment")]
-        [MaxLength(500)]
-        public string? HODComment { get; set; }
+    [Display(Name = "HOD Comment")]
+    [MaxLength(500)]
+    public string? HODComment { get; set; }
 
-        [Display(Name = "HOD Action Date")]
-        public DateTime? HODActionDate { get; set; }
+    [Display(Name = "HOD Action Date")]
+    public DateTime? HODActionDate { get; set; }
 
-        [Display(Name = "Director Action")]
-        public string? DirectorAction { get; set; }
+    [Display(Name = "Director Action")]
+    public string? DirectorAction { get; set; }
 
-        [Display(Name = "Director Comment")]
-        [MaxLength(500)]
-        public string? DirectorComment { get; set; }
+    [Display(Name = "Director Comment")]
+    [MaxLength(500)]
+    public string? DirectorComment { get; set; }
 
-        [Display(Name = "Director Action Date")]
-        public DateTime? DirectorActionDate { get; set; }
-        [Display(Name = "M & E Officer Action")]
-        public string? MEOfficerAction { get; set; }
+    [Display(Name = "Director Action Date")]
+    public DateTime? DirectorActionDate { get; set; }
+    [Display(Name = "M & E Officer Action")]
+    public string? MEOfficerAction { get; set; }
 
-        [Display(Name = "M & E Officer Comment")]
-        [MaxLength(500)]
-        public string? MEOfficerComment { get; set; }
+    [Display(Name = "M & E Officer Comment")]
+    [MaxLength(500)]
+    public string? MEOfficerComment { get; set; }
 
-        [Display(Name = "M & E Officer Action Date")]
-        public DateTime? MEOfficerActionDate { get; set; }
-        [Display(Name = "Head BPD Action")]
-        public string? HBPDAction { get; set; }
+    [Display(Name = "M & E Officer Action Date")]
+    public DateTime? MEOfficerActionDate { get; set; }
+    [Display(Name = "Head BPD Action")]
+    public string? HBPDAction { get; set; }
 
-        [Display(Name = "Head BPD Comment")]
-        [MaxLength(500)]
-        public string? HBPDComment { get; set; }
+    [Display(Name = "Head BPD Comment")]
+    [MaxLength(500)]
+    public string? HBPDComment { get; set; }
 
-        [Display(Name = "Head BPD Action Date")]
-        public DateTime? HBPDActionDate { get; set; }
-        [Display(Name = "Director DCS Action")]
-        public string? DDCSAction { get; set; }
+    [Display(Name = "Head BPD Action Date")]
+    public DateTime? HBPDActionDate { get; set; }
+    [Display(Name = "Director DCS Action")]
+    public string? DDCSAction { get; set; }
 
-        [Display(Name = "Director DCS Comment")]
-        [MaxLength(500)]
-        public string? DDCSComment { get; set; }
+    [Display(Name = "Director DCS Comment")]
+    [MaxLength(500)]
+    public string? DDCSComment { get; set; }
 
-        [Display(Name = "Director DCS Action Date")]
-        public DateTime? DDCSActionDate { get; set; }
-        [Required]
-        public virtual int? SDTMasterId { get; set; }
-        [ForeignKey("SDTMasterId")]
-        public virtual SDTMaster? SDTMasterFk { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusHOD { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusDirector { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusOfficer { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusMEOFfficer { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusHBPD { get; set; }
-        [Display(Name = "Approval Status")]
-        public int ApprovalStatusDDCS { get; set; } 
-    }
+    [Display(Name = "Director DCS Action Date")]
+    public DateTime? DDCSActionDate { get; set; }
+    [Required]
+    public virtual int? SDTMasterId { get; set; }
+    [ForeignKey("SDTMasterId")]
+    public virtual SDTMaster? SDTMasterFk { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusHOD { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusDirector { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusOfficer { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusMEOFfficer { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusHBPD { get; set; }
+    [Display(Name = "Approval Status")]
+    public int ApprovalStatusDDCS { get; set; }
+  }
 }
---
+++ MEMIS/Migrations/20241019073442_SDTAssessmentYearandQuarter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MEMIS.Migrations
+{
+    /// <inheritdoc />
+    public partial class SDTAssessmentYearandQuarter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FYear",
+                table: "SDTAssessment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Quarter",
+                table: "SDTAssessment",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FYear",
+                table: "SDTAssessment");
+
+            migrationBuilder.DropColumn(
+                name: "Quarter",
+                table: "SDTAssessment");
+        }
+    }
+}
